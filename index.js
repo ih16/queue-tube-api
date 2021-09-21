e7b2ead83f97fe754666749f69a7423cdea59936
Tonmoy_Strativ <tonmoy@strativ.se>
2021-09-21 14:34:12 +0600

    chore: added router

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const router = new Router();
+
+router.get("/", (context) => {
+  context.response.body = "Queue Tube";
+});
 
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello Oak!";
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 addEventListener("fetch", app.fetchEventHandler());
